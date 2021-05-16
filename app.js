692fce4e7a7d0d6fb5686af9ae7e4a99496d9b6a
wadep <wadepfacer@gmail.com>
2021-05-15 22:20:24 -0600

    added conditionals and set up template

--- app.js
+++ app.js
@@ -74,6 +74,75 @@ let readmeQuestions;
 const questionsHeading = '## Questions';
 let completeREADME = [];
 
+// Adding Title
+if (input.title == '') {
+    readmeTitle = '# TITLE';
+} else {
+    readmeTitle = `# ${input.title}`;
+}
+completeREADME.push(readmeTitle);
+
+//Add in license badge here!!
+let badge = `![](https://img.shields.io/badge/license-${readmeLicence.replace(/ /g, "%20")}-blue?style=flat-square)`;
+completeREADME.push(badge);
+    
+    
+// Adding description
+if (input.description == '') {
+   readmeDescription = `${descriptionHeading}\n Enter project description here.`;
+} else {
+   readmeDescription = `${descriptionHeading}\n${input.description}`;
+}
+completeREADME.push(readmeDescription);
+    
+    
+//Adding Table of Contents
+tableOfContents = `${tableOfContentsHeading}\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contribution](#contribution)\n* [Tests](#tests)\n* [License](#license)\n* [Questions](#questions)\n`;
+completeREADME.push(tableOfContents);
+    
+    
+//Adding installation instructions
+completeREADME.push(`${installHeading}`);
+    
+installArr = input.install.split(',').map(item => {
+    return `${item.trim()}`;
+});
+    
+for (var i = 0; i < installArr.length; i++) {
+    completeREADME.push(`${i + 1}. ${installArr[i]}`);
+}
+    
+    
+//Adding Usage
+if (input.usage == '') {
+    readmeUsage = `\n${usageHeading}\n Enter project usage here.`;
+} else {
+    readmeUsage = `\n${usageHeading}\n${input.usage}`;
+}
+completeREADME.push(readmeUsage);
+    
+    
+//Adding Contributing
+if (input.contribution == '') {
+    readmeContribution = `\n${contributionHeading}\n Enter project contribution information here.`;
+} else {
+    readmeContribution = `\n${contributionHeading}\n${input.contribution}`;
+}
+completeREADME.push(readmeContribution);
+
+//License info
+readmeLicence = `\n${licenseHeading}\nThis project is covered under the ${input.license}.`;
+completeREADME.push(readmeLicence);
+     
+     
+//Questions section with github link
+readmeQuestions = `\n${questionsHeading}\nFor questions about this project, please see my GitHub at [${input.github}](https://github.com/${input.github}), or reach out by email at ${input.email}.`;
+completeREADME.push(readmeQuestions);
+     
+     
+//Joining the created README Array with a newline separator
+const README = completeREADME.join('\n');
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
